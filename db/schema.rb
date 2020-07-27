e83f7e0ee3ff7ed0267fc10b148aa657210b541b
j9141997 <j9141997@gmail.com>
2020-07-27 23:43:59 +0900

    defined answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_112256) do
+ActiveRecord::Schema.define(version: 2020_07_25_141243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
