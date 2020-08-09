bd84bbfab47f40207d390746ecfcb61b093a10a2
j9141997 <j9141997@gmail.com>
2020-08-09 23:19:11 +0900

    new controller registrations for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Account', at: 'accounts'
+
   namespace 'api' do
     namespace 'v1' do
       resources :questions
       resources :answers
+      mount_devise_token_auth_for 'Account', at: 'accounts'
     end
   end
 end
